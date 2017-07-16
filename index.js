759507275d680eb3a107a97ba2985f3e9b6a15be
vinvin1022 <vin_i@foxmail.com>
2017-07-16 22:13:59 +0800

    应用集群扩展

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.post('/newsletter', function (req, res) {
     // 输入验证
     console.log(email)
     if (!email.match(VALID_EMAIL_REGEX)) {
-        if (req.xhr) return ;
+        if (req.xhr) return;
         res.json({
             error: 'Invalid name email address.'
         });
@@ -114,7 +114,7 @@ app.post('/newsletter', function (req, res) {
         email: email
     }).save(function (err) {
         if (err) {
-            if (req.xhr) return ;
+            if (req.xhr) return;
             res.json({
                 error: 'Database error.'
             });
@@ -213,7 +213,41 @@ app.use(function (req, res, next) {
     res.send("<h1 style='text-align:center;color:#ff0000'>没有找到相关页面！</h1>");
 });
 
+switch (app.get('env')) {
+    case 'development':
+        // 紧凑的、彩色的开发日志
+        app.use(require('morgan')('dev'));
+        break;
+    case 'production':
+        // 模块 'express-logger' 支持按日志循环
+        app.use(require('express-logger')({
+            path: __dirname + '/log/requests.log'
+        }));
+        break;
+}
+
 //监听端口
-app.listen(app.get("port"), function () {
-    console.log('App listening on port 3000!');
-});
+// app.listen(app.get("port"), function () {
+//     console.log('Express started in ' + app.get('env') +
+//         ' mode on http://localhost:' + app.get('port') +
+//         '; press Ctrl-C to terminate.');
+// });
+
+
+function startServer() {
+    app.listen(app.get('port'), function () {
+        console.log('Express started in ' + app.get('env') +
+            ' mode on http://localhost:' + app.get('port') +
+            '; press Ctrl-C to terminate.');
+    });
+}
+if (require.main === module) {
+    // 应用程序直接运行；启动应用服务器
+    console.log("------------" + require.main + "\n " + module)
+    startServer();
+} else {
+    // 应用程序作为一个模块通过 "require" 引入 : 导出函数
+    // 创建服务器
+    console.log("我已经来过index了");
+    module.exports = startServer;
+}
